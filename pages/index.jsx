2626435f116db525ef820c8ec9908b7a35905773
VulcanWM <logmedha09@gmail.com>
2023-04-10 16:40:35 +0100

    added a logout button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,20 @@
 import Layout from '../components/layout'
-import { setCookie, getCookie } from 'cookies-next';
+import { setCookie, getCookie, deleteCookie } from 'cookies-next';
+import Router from "next/router";
+
+function logout(){
+    deleteCookie("username");
+    Router.reload()
+}
 
 export default function HomePage( {username} ) {
     return (
         <Layout pageTitle="Home">
         {username ?
-        <h2>Hi {username}</h2>: 
+        <>
+            <h2>Hi {username}</h2>
+            <button onClick={logout}>Logout</button>
+        </>: 
         <>
             <h2>Log in</h2>
             <form action='/api/logged-in' method='POST'>
